653753e4245982a1eeabf9b5b7e801fa28423cb7
Henry Martinez <7996045+martinezhenry@users.noreply.github.com>
2021-11-25 10:52:23 -0500

    add abstract base

---
+++ command/src/main/java/com/hvs/design/patterns/command/Command.java
@@ -0,0 +1,25 @@
+package com.hvs.design.patterns.command;
+
+/**
+ * Command
+ * <p>
+ * Command class.
+ * <p>
+ * <p>
+ * ESTE COMPONENTE FUE CONSTRUIDO SIGUIENDO LOS ESTANDARES DE DESARROLLO Y EL PROCEDIMIENTO
+ * DE DESARROLLO DE APLICACIONES DE NOVOPAYMENT Y SE ENCUENTRA PROTEGIDO POR LAS LEYES DE
+ * PROPIEDAD INTELECTUAL Y DERECHOS DE AUTOR.
+ *
+ * @author Novopayment Inc.
+ * @author hmartinez@novopayment.com
+ * @since 25/11/21
+ */
+public abstract class Command {
+
+    Command(){
+
+    }
+
+    public abstract boolean execute();
+
+}
